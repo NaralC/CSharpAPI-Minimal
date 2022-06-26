841679405f2e65506ebb452e3b8fbca66202d0ea
Naral <you@example.com>
2022-06-26 22:24:34 +0700

    Added Dto's

---
+++ Dtos/CommandCreateDto.cs
@@ -0,0 +1,23 @@
+// DTO = Data Transfer Object
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Dtos
+{
+    public class CommandCreateDto
+    {
+        // We do not need this since our database, by default, creates Id's for us
+        // [Key]
+        // public int Id { get; set; }
+
+        [Required]
+        public string? HowTo { get; set; }
+
+        [Required]
+        [MaxLength(5)]
+        public string? Platform { get; set; }
+
+        [Required]
+        public string? CommandLine { get; set; }
+    }
+}
