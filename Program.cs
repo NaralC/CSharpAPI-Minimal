2542b52a60d0b02a49ace27285eb467bddc44e6c
Naral <you@example.com>
2022-07-06 23:51:35 +0700

    Update Swagger + Add Migration

--- Program.cs
+++ Program.cs
@@ -25,11 +25,8 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); // Regi
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
